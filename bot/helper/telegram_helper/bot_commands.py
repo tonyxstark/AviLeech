2c7c0caa6b087426151dbc37193abaac5ebccf24
Mr. Stark <75563336+tonyxstark@users.noreply.github.com>
2021-08-23 20:44:06 +0530

    Add files via upload

---
+++ bot/helper/telegram_helper/bot_commands.py
@@ -0,0 +1,23 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = 'startx'
+        self.MirrorCommand = 'mirrorx'
+        self.UnzipMirrorCommand = 'unzipmirrorx'
+        self.TarMirrorCommand = 'tarmirrorx'
+        self.CancelMirror = 'cancelx'
+        self.CancelAllCommand = 'cancelallx'
+        self.ListCommand = 'listx'
+        self.StatusCommand = 'statusx'
+        self.AuthorizeCommand = 'authorize'
+        self.UnAuthorizeCommand = 'unauthorize'
+        self.PingCommand = 'pingx'
+        self.RestartCommand = 'restartx'
+        self.StatsCommand = 'statsx'
+        self.HelpCommand = 'helpx'
+        self.LogCommand = 'logx'
+        self.CloneCommand = "clonex"
+        self.WatchCommand = 'watchx'
+        self.TarWatchCommand = 'tarwatchx'
+        self.deleteCommand = 'delx'
+
+BotCommands = _BotCommands()
